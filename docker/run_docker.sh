983a15f6cd2389ad10d4810a0e84bf6ce946ca56
jack-scott <jack.scott@hullbot.com>
2021-05-22 15:55:04 +1000

    fixed docker file, working now

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -6,4 +6,4 @@ SCRIPT_LOC=$(readlink -f $0)
 SCRIPT_FOLDER=$(dirname $SCRIPT_LOC)
 REPO_LOC=$SCRIPT_FOLDER/.. #need to go one path back as this is in the docker folder
 
-docker run --rm -it --name basicwebsite --volume $REPO_LOC:/home/user/basic_website alpine/basic_website bash
+docker run --rm -it --name basicwebsite --net host --volume $REPO_LOC:/home/user/basic_website alpine/basic_website bash
